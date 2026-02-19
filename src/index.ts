b706f547829fe51713d4b55c556882a04820ad63
Brian Kiplagat <kiplagatbrian18@gmail.com>
2026-02-19 18:00:42 +0300

    feat: integrate AccordionController for enhanced accordion functionality in the gallery

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import '$styles/accordion.css';
 import '$styles/gallery.css';
 
+import { AccordionController } from '$utils/accordion';
 import { GalleryController } from '$utils/gallery';
 import { StickyNavController } from '$utils/sticky-nav';
 
@@ -10,4 +12,7 @@ window.Webflow.push(() => {
 
   const galleryController = new GalleryController();
   galleryController.init();
+
+  const accordionController = new AccordionController();
+  accordionController.init();
 });
